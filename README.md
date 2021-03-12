590d7646c973365050d7edb91d6d2a3fb7ffc387
mkellsy <mkellsy@gmail.com>
2021-03-11 20:25:47 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# pam
+# ![](https://raw.githubusercontent.com/hoobs-org/HOOBS/master/docs/logo.png)
+
 Asynchronous PAM authentication for NodeJS
+
+It tries to superseed the previous and outdated node-pam extension with the following improvements:
+* Allows to provide own service name, for example common-auth or any custom service name defined in `/etc/pam.d`
+* Allows to provide PAM_RHOST via 'remoteHost' option. It is used to provide remote network authentication that will skip any local only authentication methods like for example fingerprint reading.
+* Already mentioned utilization of libuv and node-gyp
+* Proper type checking in C++ code, it throws exception if bad types are given
+* In case of error it passes the error string containing both pam function and `pam_strerror()` results
+
+## Usage
+
+Simple usage
+------------
+Default service_name for `pam_start(2)` is 'login'.
+
+```js
+var pam = require("@hoobs/pam");
+
+pam.authenticate("username", "password", (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("authenticated");
+    }
+});
+```
+
+## Options
+Proper apps should provide their own service name. Sample services are located in `/etc/pam.d`. As an example lookup a service name file for `sshd`. To do proper network authentication you should also provide `remoteHost` key to the options argument. It will be passed to pam as `PAM_RHOST` (`pam_set_item(2)`).
+
+```js
+var pam = require("@hoobs/pam");
+
+pam.authenticate("username", "password", (error) => {
+    if (error) {
+        console.log(errpr);
+    } else {
+        console.log("Authenticated!");
+    }
+}, {
+    serviceName: "myapp",
+    remoteHost: "localhost",
+});
+```
+
+## Installation
+First you will need to install PAM libraries.
+
+```
+sudo apt install libpam-dev
+```
+
+Then you can install this module into your project.
+```
+yarn add @hoobs/pam
+```
+
+## Credits
+This is a modified project orginally wrote by [Damian Kaczmarek](https://github.com/Rush).
---
+++ build/Release/.deps/Release/obj.target/authenticate_pam/authenticate_pam.o.d
@@ -0,0 +1,68 @@
+cmd_Release/obj.target/authenticate_pam/authenticate_pam.o := g++ '-DNODE_GYP_MODULE_NAME=authenticate_pam' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/hoobs/.cache/node-gyp/14.16.0/include/node -I/home/hoobs/.cache/node-gyp/14.16.0/src -I/home/hoobs/.cache/node-gyp/14.16.0/deps/openssl/config -I/home/hoobs/.cache/node-gyp/14.16.0/deps/openssl/openssl/include -I/home/hoobs/.cache/node-gyp/14.16.0/deps/uv/include -I/home/hoobs/.cache/node-gyp/14.16.0/deps/zlib -I/home/hoobs/.cache/node-gyp/14.16.0/deps/v8/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/authenticate_pam/authenticate_pam.o.d.raw   -c -o Release/obj.target/authenticate_pam/authenticate_pam.o ../authenticate_pam.cc
+Release/obj.target/authenticate_pam/authenticate_pam.o: \
+ ../authenticate_pam.cc ../node_modules/nan/nan.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/node_version.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/uv.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/uv/errno.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/uv/version.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/uv/unix.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/uv/threadpool.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/uv/linux.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/node.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/v8.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/cppgc/common.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/v8config.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/v8-internal.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/v8-version.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/v8config.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/v8-platform.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/node_version.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/node_buffer.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/node.h \
+ /home/hoobs/.cache/node-gyp/14.16.0/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h
+../authenticate_pam.cc:
+../node_modules/nan/nan.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/node_version.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/uv.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/uv/errno.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/uv/version.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/uv/unix.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/uv/threadpool.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/uv/linux.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/node.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/v8.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/cppgc/common.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/v8config.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/v8-internal.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/v8-version.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/v8config.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/v8-platform.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/node_version.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/node_buffer.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/node.h:
+/home/hoobs/.cache/node-gyp/14.16.0/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
